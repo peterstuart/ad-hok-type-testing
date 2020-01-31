abead359530ea6f7d273b3ad7273a89740938b11
Julian Rosse <julian@helixbass.net>
2020-01-31 14:21:45 -0800

    function of props state initializer; omitted initial value example

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,48 @@
-import React from "react"
-import { addState } from "ad-hok"
-import { flow } from "lodash/fp"
+import React, { FC } from 'react'
+import { addState } from 'ad-hok'
+import { flow } from 'lodash/fp'
+
+interface AddStateInitialStateAsCallbackProps {
+  name: string
+}
+
+const AddStateInitialStateAsCallback: FC<AddStateInitialStateAsCallbackProps> = flow(
+  addState('value', 'setValue', ({ name }) => 'name: ' + name),
+  // addState<number>('num', 'setNum'),
+  addState('num', 'setNum', undefined as number | undefined),
+  ({ name, value, num, setNum }) => (
+    <div>
+      <div>name: {name}</div>
+      <div>value: {value}</div>
+      <div>num: {num}</div>
+      <button
+        onClick={() =>
+          setNum(
+            // 'a' // correctly disallowed
+            3,
+          )
+        }
+      >
+        set num
+      </button>
+    </div>
+  ),
+)
 
 interface AppProps {
   externalProp: string
 }
 
-const App: React.FC<AppProps> = flow(
-  addState("name", "setName", "hello"),
+const App: FC<AppProps> = flow(
+  addState('name', 'setName', 'hello'),
   ({ name, setName, externalProp }) => (
     <div>
-      <span>External prop: {externalProp}</span>
-      <span>Name: {name}</span>
-      <button onClick={() => setName("abc")}>set name</button>
+      <div>External prop: {externalProp}</div>
+      <div>Name: {name}</div>
+      <button onClick={() => setName('abc')}>set name</button>
+      <AddStateInitialStateAsCallback name={name} />
     </div>
-  )
+  ),
 )
 
 export default App
--- types/ad-hok/index.d.ts
+++ types/ad-hok/index.d.ts
@@ -1,14 +1,15 @@
-declare module "ad-hok" {
+declare module 'ad-hok' {
   type AddStateType = <
     TState,
     TStateName extends string,
-    TStateUpdaterName extends string
+    TStateUpdaterName extends string,
+    TProps
   >(
     stateName: TStateName,
     stateUpdaterName: TStateUpdaterName,
-    initialState: TState
-  ) => <TProps>(
-    props: TProps
+    initialState: TState | ((props: TProps) => TState),
+  ) => (
+    props: TProps,
   ) => TProps &
     { [stateName in TStateName]: TState } &
     { [stateUpdaterName in TStateUpdaterName]: (state: TState) => void }
